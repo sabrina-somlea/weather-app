a0da037fdcf23c8acee7c6f202b24055875c6e02
Sabrina-Daniela Groza <sabrina.somlea@gmail.com>
2023-04-06 00:46:50 +0300

    fixed weather location

--- weather-app/src/app/weather.service.ts
+++ weather-app/src/app/weather.service.ts
@@ -1,17 +1,24 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpParams} from "@angular/common/http";
+import {resolve} from "@angular/compiler-cli";
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
-  apiKey='47bdd22832aa91c7ba1cf4f40dc126d3';
-  constructor(private http:HttpClient) { }
-  // getWeather(city: string){
-  //   return this.http.get('http://api.openweathermap.org/geo/1.0/direct?q={city}&appid={47bdd22832aa91c7ba1cf4f40dc126d3}')
-  // }
-  getWeather(){
-    return this.http.get('http://api.openweathermap.org/data/2.5/weather?lat=46.7667&lon=23.6&appid=47bdd22832aa91c7ba1cf4f40dc126d3');
-  };
+  apiKey = '47bdd22832aa91c7ba1cf4f40dc126d3';
+
+
+  constructor(private http: HttpClient) {
+  }
+
+
+
+  getWeather(lat:string, lng:string){
+    return this.http.get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${this.apiKey}&units=metric`);
+  }
+  ;
+
+
 
 }
--- weather-app/src/app/weather/weather.component.ts
+++ weather-app/src/app/weather/weather.component.ts
@@ -7,16 +7,43 @@ import {WeatherService} from "../weather.service";
   styleUrls: ['./weather.component.css']
 })
 export class WeatherComponent {
-city!:string;
+// city!:string;
 weatherData: any;
+lat: any;
+lng: any;
+
+
+
 constructor (private weatherService:WeatherService){}
 
-  ngOnInit(){}
-  getWeather(){
-    this.weatherService.getWeather().subscribe(data=>{
-      this.weatherData=data;
-      console.log(data);
-    })
-    }
 
-}
+  public ngOnInit() {
+    this.getLocation();
+  }
+
+  getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position: any) => {
+        if (position) {
+          console.log("Latitude: " + position.coords.latitude + "Longitude: " + position.coords.longitude);
+          this.lat = position.coords.latitude;
+          this.lng = position.coords.longitude;
+          console.log(this.lat);
+          console.log(this.lng);
+          this.weatherService.getWeather(this.lat,this.lng).subscribe(data => {
+            this.weatherData = data;
+          });
+        }
+      }, (error: any) => console.log(error));
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
+  }
+
+
+  }
+
+
+
+
+
